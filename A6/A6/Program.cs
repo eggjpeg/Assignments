de9993ef05b948ca64ecc60d1c8106d1dc745958
egg jpeg <kolya46592@gmail.com>
2019-01-06 20:11:56 +0100

    spaz

---
+++ A6/A6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace A6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        static string Compress(string s)
+        {
+            foreach (char dupe in s)
+            {
+                if (s[dupe] == s[dupe + 1])
+                {
+
+                }
+            }
+        }
+    }
+}
