b442e0cb4a86b0f800ebe4bbf3e71358100af26f
egg jpeg <kolya46592@gmail.com>
2019-01-10 00:26:27 +0100

    final complete

--- A6/A6/Program.cs
+++ A6/A6/Program.cs
@@ -10,10 +10,39 @@ namespace A6
     {
         static void Main(string[] args)
         {
-          string spaz =  Compress("AAAAAAAABBBCD");
+            string s = LoadString("txt.txt");
+            string spaz = Compress(s);
+
             Console.WriteLine(spaz);
             Console.ReadLine();
         }
+
+
+        static string DeCompress(string s)
+        {
+            StringBuilder sb = new StringBuilder();
+            char c = ' ';
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (char.IsLetter(s[i]))
+                {
+                    c = s[i];
+                    sb.Append(s[i]);
+                }
+                else if (char.IsNumber(s[i]))
+                {
+                    string sc = Char.ConvertFromUtf32(s[i]);
+                    int loop = int.Parse(sc);
+
+                    for (int j = 1; j <= loop - 1; j++)
+                        sb.Append(c);
+                }
+                else
+                    continue;
+            }
+            return sb.ToString();
+        }
+
         static string Compress(string s)
         {
             StringBuilder sb = new StringBuilder();
@@ -39,6 +68,50 @@ namespace A6
             }
 
             return sb.ToString();
+        }
+        //static string CompressFile(string file)
+        //{
+        //    using (StreamReader sr = new StreamReader(file))
+        //    {
+        //        var list = new List<string>();
+        //        while (!sr.EndOfStream)
+        //        {
+        //            int acc = 1;
+
+        //            s = s.PadRight(' ');
+        //            for (int i = 0; i < s.Length - 1; i++)
+        //            {
+        //                if (s[i] == s[i + 1])
+        //                    acc++;
+        //                else
+        //                {
+        //                    if (acc != 1)
+        //                    {
+        //                        list.Add(s[i] + Convert.ToString(acc) + "~");
+        //                        acc = 1;
+        //                    }
+        //                    else
+        //                        sb.Append(s[i]);
+
+        //                }
+        //            }
+        //        }
+        //    }
+        //}
+        static string LoadString(string file)
+        {
+            using (StreamReader sr = new StreamReader(file))
+            {
+                StringBuilder sb = new StringBuilder();
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine().Trim().ToLower();
+                    if (line == "")
+                        continue;
+                    sb.Append(line);
+                }
+                return sb.ToString();
+            }
         }
     }
 }
