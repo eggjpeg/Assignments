564ac93ac98dafc9e30786452078b868c41c5192
egg jpeg <kolya46592@gmail.com>
2019-01-11 14:30:39 +0100

    Encrypt done

--- A7/A7/Program.cs
+++ A7/A7/Program.cs
@@ -10,30 +10,31 @@ namespace A7
     {
         public static void Main(string[] args)
         {
-            string spaz = Encrypt("x");
+            string spaz = Encrypt("spaz");
             Console.WriteLine(spaz);
             Console.ReadLine();
         }
+
         static string Encrypt(string s)
         {
             StringBuilder sb = new StringBuilder();
             var list = new List<char>();
-            var dict = new Dictionary<string, string>();
-            char[] alpha = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
-            //dict.Add(alpha);
-            for (int i = 0; i < s.Length - 1; i++)
-            {
-                for (int j = 0;j < list.Count; j++)
-                {
-                    if(s[i] == list[24])
-                        sb.Append(list[0]);
-                     else if (list[j] == 'y')
-                        sb.Append(list[1]);
-                     else if (s[i] == 'z')
-                        sb.Append(list[2]);
-                    else if (s[i] == list[j])
-                        sb.Append(list[j + 3]);
+            string alpha  = "abcdefghijklmnopqrstuvwxyz";
+            string enalpha = "defghijklmnopqrstuvwxyzabc";
 
+            string alphaCaps = alpha.ToUpper();
+            string enalphaCaps = enalpha.ToUpper();
+            
+
+            for (int i = 0; i < s.Length; i++)
+            {
+
+                for (int j = 0; j < alpha.Length; j++)
+                {
+                    if(s[i] == alpha[j])
+                        sb.Append(enalpha[j]);
+                    else if(s[i] == alphaCaps[j])
+                        sb.Append(enalphaCaps[j]);
                 }
             }
             return sb.ToString();
