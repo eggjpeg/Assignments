89290892615a29f957d51e9387ed16b99fc9e654
egg jpeg <kolya46592@gmail.com>
2019-01-13 16:40:25 +0100

    final i think

---
+++ A8/A8/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetTwins(15));
+            Console.ReadLine();
+        }
+        static bool IsPrime(int n)
+        {
+            for (int i = 2; i < n; i++)
+            {
+                if (n % i == 0)
+                    return false;
+            }
+            return true;
+        }
+        static List<int> GetPrimes(int n)
+        {
+            var list = new List<int>();
+            for (int i = 2; i <= n; i++)
+            {
+                if (IsPrime(i) == true)
+                    list.Add(i);
+            }
+            return list;
+        }
+        static List<Tuple<int,int>> GetTwins(int n)
+        {
+            List<int> list = GetPrimes(n);
+            var lst = new List<Tuple<int, int>>();
+            for (int i = 0; i <= list.Count - 2; i++)
+            {
+                lst.Add(Tuple.Create(list[i], list[i + 1]));
+            }
+            return lst;
+        }
+
+    }
+}
