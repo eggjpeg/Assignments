bc9a91f7599c1f07c7ef80441a47765e380bb7bb
egg jpeg <kolya46592@gmail.com>
2019-01-17 21:41:21 +0100

    final

--- A9/A9/Program.cs
+++ A9/A9/Program.cs
@@ -11,29 +11,49 @@ namespace A9
         static void Main(string[] args)
         {
             List<object> list = Tokenize("22+11-3-9-10");
+            Evaluate(list);
             Console.ReadLine();
         }
         static List<object> Tokenize(string exp)
         {
-           // double spaz = 0;
+            StringBuilder sb = new StringBuilder();
             var list = new List<object>();
             for (int i = 0; i < exp.Length; i++)
             {
-
                 bool isInt = double.TryParse(exp[i].ToString(), out double spaz);
-                bool isInt2 = double.TryParse(exp[i].ToString(), out double spaz2);
-
-                //  string[] n = exp.Split('+', '-');
-                if (isInt == true)
+                if (isInt)
+                    sb.Append(exp[i]);
+                else
                 {
-                    double n = (double)char.GetNumericValue(num);
-                    list.Add(n);
-                }
-                if (isInt == false)
+                    if(sb.Length>0)
+                        list.Add(int.Parse(sb.ToString()));
                     list.Add(exp[i]);
+                    sb.Clear();
+                }
             }
-               
+            if (sb.Length > 0)
+                list.Add(int.Parse(sb.ToString()));
             return list;
+        }
+        static List<object> Evaluate(List<object> list)
+        {
+            int n1 = 0;
+            int n2 = 0;
+            char op = ' ';
+            var lstNew = new List<object>();
+            if (list.Count == 1)
+                return list;
+            n1 = (int)list[0];
+                    op = (char)list[1];
+                    n2 = (int)list[2];
+                    if (op == '+')
+                        lstNew.Add(n1+n2);
+                    else
+                        lstNew.Add(n1 - n2);
+            List<object> lstRemainder = list.GetRange(3, list.Count - 3);
+            lstNew.AddRange(lstRemainder);
+            
+             return Evaluate(lstNew);
         }
     }
 }
