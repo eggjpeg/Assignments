9a72ecdceb28fec88841b6168312e141cdfbd514
egg jpeg <kolya46592@gmail.com>
2019-01-26 15:57:55 +0100

    commit spaz

---
+++ A11/A11/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExpEval e = new ExpEval();
+            //double r = e.Evaluate("cos(4*5)*2+sin(37)-tan(44)");
+            double r = e.Evaluate("log(cos(tan(45*2)))");
+            Console.WriteLine(r);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ A11/A11/Util.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace A11
+{
+    class Util
+    {
+        public static bool IsDouble(char str)
+        {
+            return IsDouble(str.ToString());
+        }
+
+        public static bool IsDouble(string str)
+        {
+            return double.TryParse(str.ToString(), out double spaz);
+        }
+        public static int FindSmallestIndexOf(List<object> list, char op1, char op2)
+        {
+            int i1 = list.IndexOf(op1) == -1 ? int.MaxValue : list.IndexOf(op1);
+            int i2 = list.IndexOf(op2) == -1 ? int.MaxValue : list.IndexOf(op2);
+            if (i1 < i2)
+                return i1;
+            else
+                return i2;
+        }
+        public static Tuple<int, int> FindInnerPars(List<object> list, char par1, char par2)
+        {
+            int i1 = list.LastIndexOf(par1) == -1 ? int.MaxValue : list.LastIndexOf(par1);
+            int i2 = list.IndexOf(par2, i1) == -1 ? int.MaxValue : list.IndexOf(par2, i1);
+            var tuple = new Tuple<int, int>(i1, i2);
+            return tuple;
+        }
+        public static Tuple<int, int> FindClosestPars(List<object> list, char par1, char par2, int i)
+        {
+            int i1 = list.IndexOf(par1, i) == -1 ? int.MaxValue : list.IndexOf(par1);
+            int i2 = list.IndexOf(par2, i1) == -1 ? int.MaxValue : list.IndexOf(par2, i1);
+            var tuple = new Tuple<int, int>(i1, i2);
+            return tuple;
+        }
+
+    }
+}
