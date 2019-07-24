c0d92956ababe5581e6ad0e55c758a699a358de6
eggjpeg <46425246+eggjpeg@users.noreply.github.com>
2019-07-24 12:24:49 -0400

    WordTree

---
+++ WordTree/WordTree/Node.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordTree
+{
+    class Node
+    {
+        public string value;
+        public List<Node> Children;
+        public void AddChild(Node n)
+        {
+            if (Children == null)
+                Children = new List<Node>();
+            Children.Add(n);
+        }
+
+        public void PrintPretty(string indent, bool last)
+        {
+            Console.Write(indent);
+            if (last)
+            {
+                Console.Write("\\-");
+                indent += "  ";
+            }
+            else
+            {
+                Console.Write("|-");
+                indent += "| ";
+            }
+
+            Console.WriteLine(value);
+
+            if (Children == null)
+                return;
+
+            for (int i = 0; i < Children.Count; i++)
+                Children[i].PrintPretty(indent, i == Children.Count - 1);
+        }
+    }
+}
---
+++ WordTree/WordTree/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace WordTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node r = new Node();
+            r.value = "_";
+            InsertToFile("text.txt");
+            Console.ReadLine();
+        } 
+
+        static public void InsertToFile(string file)
+        {
+            Node r = new Node();
+            using (StreamReader sr = new StreamReader(file))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    Insert(r, line.ToLower(), 0);
+                }
+                r.PrintPretty(" ", true);
+            }
+        }
+
+        static public void Insert(Node currentNode, string word, int i)
+        {
+            bool valueExists = false;
+            Node childNode = new Node();
+
+            if (i == word.Length || i == 4)
+            {
+                currentNode.value = word;
+                return;
+            }
+            if (currentNode.Children != null)
+            {
+                foreach (var item in currentNode.Children)
+                {
+                    if (item.value == word[i].ToString())
+                    {
+                        valueExists = true;
+                        childNode = item;
+                        break;
+                    }
+                }
+            }
+            if (valueExists == false)
+            {
+                childNode.value = word[i].ToString();
+                currentNode.AddChild(childNode);
+            }
+            Insert(childNode, word, i + 1);
+        }
+
+    }
+}
