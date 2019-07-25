2a577c52785b793224348365763004e6964d3cf6
eggjpeg <46425246+eggjpeg@users.noreply.github.com>
2019-07-25 15:31:24 -0400

    wordtree done

--- WordTree/WordTree/Program.cs
+++ WordTree/WordTree/Program.cs
@@ -4,22 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
-
+using System.Diagnostics;
+using System.Threading;
 namespace WordTree
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Node r = new Node();
-            r.value = "_";
-            InsertToFile("text.txt");
+            var subList = LoadList("subText.txt");
+            // var list = LoadList("text.txt");
+            Node r = LoadTree("text.txt");
+            // ContainsList(subList, list);
+             TimedContains(subList, r);
             Console.ReadLine();
-        } 
+        }
 
-        static public void InsertToFile(string file)
+        static public Node LoadTree(string file)
         {
             Node r = new Node();
+            r.value = "_";
             using (StreamReader sr = new StreamReader(file))
             {
                 while (!sr.EndOfStream)
@@ -27,10 +31,9 @@ namespace WordTree
                     string line = sr.ReadLine();
                     Insert(r, line.ToLower(), 0);
                 }
-                r.PrintPretty(" ", true);
+                return r;
             }
         }
-
         static public void Insert(Node currentNode, string word, int i)
         {
             bool valueExists = false;
@@ -60,6 +63,57 @@ namespace WordTree
             }
             Insert(childNode, word, i + 1);
         }
+
+        static public bool ContainsTree(Node currentNode, string word, int i)
+        {
+                foreach (var child in currentNode.Children)
+                {
+                    if (child.value == word)
+                        return true;
+                 
+                    if (child.value == word[i].ToString())
+                        return ContainsTree(child, word, i + 1);
+                }
+            return false;
+        }
+
+        static public void TimedContains(List<string> subList, Node r)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            foreach (var word in subList)
+            {
+                if(ContainsTree(r, word, 0))  
+                     Console.WriteLine(word);
+            }
+            sw.Stop();
+            Console.WriteLine(sw.Elapsed);
+        }
+        static public List<string> LoadList(string file)
+        {
+            var list = new List<string>();
+            using (StreamReader sr = new StreamReader(file))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    list.Add(line);
+                }
+                return list;               
+            }
+        }
+
+        static public void ContainsList(List<string> subList, List<string> list)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            foreach (var word in subList)
+                list.Contains(word);
+            sw.Stop();
+            Console.WriteLine(sw.Elapsed);           
+        }
+
+       
 
     }
 }
