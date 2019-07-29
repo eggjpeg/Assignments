e31e358fb7a20d103a9fc3e8c2aafe29fda0d97e
eggjpeg <46425246+eggjpeg@users.noreply.github.com>
2019-07-29 15:48:48 -0400

    second word tree farmed

---
+++ WordTree2/WordTree2/Node.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordTree2
+{
+	class Node
+	{
+		public string value;
+		public Node L;
+		public Node R;
+		public Node M;
+		//public void AddChild(Node n)
+		//{
+		//	if (Children == null)
+		//		Children = new List<Node>();
+		//	Children.Add(n);
+		//}
+
+         
+		public void PrintPretty(string indent, bool last)
+		{
+			Console.Write(indent);
+			if (last)
+			{
+				Console.Write("\\-");
+				indent += "  ";
+			}
+			else
+			{
+				Console.Write("|-");
+				indent += "| ";
+			}
+            Console.WriteLine(value);
+
+            if (L != null)
+            {
+               Console.Write("L");
+                L.PrintPretty(indent, false);
+            }
+            if (M != null)
+            {
+                Console.Write("M");
+                M.PrintPretty(indent, false);
+            }
+            if (R != null)
+            {
+               Console.Write("R");
+                R.PrintPretty(indent, false);
+            }
+            if (L == null && R == null && M == null )
+				return;
+
+        }
+	}
+}
---
+++ WordTree2/WordTree2/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+
+namespace WordTree2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LoadTree("/Users/n/Projects/WordTree2/WordTree2/test.txt");
+            Console.ReadLine();
+        }
+
+        public static void Insert(Node currentNode, string word, int n)
+        {
+            Node childNode = new Node();
+            char[] alphabet = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+            if (n == word.Length)
+            {
+                currentNode.value += word;
+                return;
+            }
+                for (int i = 0; i < alphabet.Length; i++)
+                {
+                    if (word[n] == alphabet[i])
+                    {
+                        if (i < 9)
+                        {
+                        if (currentNode.L == null)
+                            currentNode.L = childNode;
+                        else
+                            childNode = currentNode.L;
+                        }
+                    else if (i > 8 && i < 18)
+                        {
+                        if (currentNode.M == null)
+                            currentNode.M = childNode;
+                        else
+                            childNode = currentNode.M;
+                        }
+                        else
+                        {
+                        if (currentNode.R == null)
+                            currentNode.R = childNode;
+                        else
+                            childNode = currentNode.R;
+                        }
+
+                    }
+                }
+            Insert(childNode, word, n + 1);
+        }
+
+        static public Node LoadTree(string file)
+        {
+            Node r = new Node();
+            using (StreamReader sr = new StreamReader(file))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    Insert(r, line.ToLower(), 0);
+                }
+                r.PrintPretty(" ", true);
+
+                return r;
+            }
+        }
+    }
+}
