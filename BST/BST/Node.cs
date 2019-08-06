a812f3a0c9824a641775776927c3945e54254c0c
eggjpeg <46425246+eggjpeg@users.noreply.github.com>
2019-08-06 10:45:05 -0400

    BinarySearchTree
    farm

---
+++ BST/BST/Node.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BST
+{
+    class Node
+    {
+        public int value;
+        public Node L;
+        public Node R;
+        public Node(int value)
+        {
+            this.value = value;
+        }
+
+        public void PrintPretty(string indent, bool last)
+        {
+            Console.Write(indent);
+            if (last)
+            {
+                Console.Write("\\-");
+                indent += "  ";
+            }
+            else
+            {
+                Console.Write("|-");
+                indent += "| ";
+            }
+            Console.WriteLine(value);
+
+            if (L != null)
+            {
+                Console.Write("L");
+                L.PrintPretty(indent, false);
+            }
+            if (R != null)
+            {
+                Console.Write("R");
+                R.PrintPretty(indent, false);
+            }
+            if (L == null && R == null)
+                return;
+
+        }
+
+        public bool IsLeaf()
+        {
+            return L == null && R == null;
+        }
+
+    }
+}
---
+++ BST/BST/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node r = new Node(0);
+            var list = GenerateRandomList(100);
+            for (int i = 0; i < list.Count; i++)
+                Insert(r, list[i]);
+            r.PrintPretty(" ", true);
+        }
+
+        static List<int> GenerateRandomList(int n)
+        {
+            var list = new List<int>();
+            for (int i = 0; i < n; i++)
+            {
+                Random r = new Random();
+                list.Add(r.Next(1, 10000));
+            }
+            return list;
+        }
+        static void Insert(Node curNode, int n)
+        {
+            bool IsR = false;
+            if(n > curNode.value)
+            {
+                IsR = true;
+                if (curNode.R == null)
+                {
+                    curNode.R = new Node(n);
+                    return;
+                }
+            }
+            else
+            {
+                if (curNode.L == null)
+                {
+                    curNode.L = new Node(n);
+                    return;
+                }
+            }
+            if (IsR == true)
+                Insert(curNode.R, n);
+            else
+                Insert(curNode.L, n);
+
+        }
+
+    }
+}
