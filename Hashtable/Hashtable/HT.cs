954c747cddc745be167e15981957d2781a85ab78
eggjpeg <46425246+eggjpeg@users.noreply.github.com>
2019-08-19 23:14:51 -0400

    hashtables

---
+++ Hashtable/Hashtable/HT.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hashtable
+{
+
+    class ValueList: List<Tuple<string, string>>
+    {
+    }
+
+    class HT
+    {
+        ValueList[] valueListAr;
+        int capacity;
+
+        public HT(int capacity)
+        {
+            this.capacity = capacity;
+            valueListAr = new ValueList[capacity];
+        }
+
+        public int GetHashOf(string key)
+        {
+            return Math.Abs(key.GetHashCode()) % capacity;
+        }
+
+        private Tuple<string,bool> GetTup(string key)
+        {
+            var hashIndex = GetHashOf(key);
+            if (valueListAr[hashIndex] == null)
+                return new Tuple<string, bool>(null,false);
+
+            foreach (var item in valueListAr[hashIndex])
+                if (item.Item1 == key)
+                    return new Tuple<string, bool>(item.Item2, true); 
+            return new Tuple<string, bool>(null, false);
+        }
+
+
+        public string Get(string key)
+        {
+            return GetTup(key).Item1;
+        }
+
+        public bool ContainsKey(string key)
+        {
+            return GetTup(key).Item2;
+        }
+
+
+        public bool Add(string key, string value)
+        {
+            if (ContainsKey(key))
+                return false;
+
+            var hashIndex = GetHashOf(key);
+
+            if (valueListAr[hashIndex] == null)
+                valueListAr[hashIndex] = new ValueList();
+
+            valueListAr[hashIndex].Add(new Tuple<string, string>(key, value));
+            return true;
+        }
+
+
+        public void Print()
+        {
+            foreach (var item in valueListAr)
+                if(item != null)
+                    foreach (var innerItem in item)
+                        Console.WriteLine(innerItem.Item1 + " - " + innerItem.Item2);          
+        }
+    }
+}
---
+++ Hashtable/Hashtable/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hashtable
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            
+            var list = LoadList("20k.txt");
+            HT ht = new HT(20000);
+            //Dictionary<string, string> d = new Dictionary<string, string>();
+
+
+
+            Stopwatch sw = new Stopwatch();
+
+            sw.Start();
+            //foreach (var item in list)
+            //    d.Add(item, item);
+            sw.Stop();
+
+            Console.WriteLine(sw.Elapsed + " theirs load ");
+
+            sw.Reset();
+
+            sw.Start();
+            foreach (var item in list)
+                ht.Add(item, item);
+            sw.Stop();
+            
+            Console.WriteLine(sw.Elapsed + " ours load ");
+
+            sw.Reset();
+            list.Sort();
+            sw.Start();
+            for (int i = 0; i < 100; i++)
+                foreach (var item in list)
+                    list.BinarySearch(item);
+            sw.Stop();
+            Console.WriteLine(sw.Elapsed + " bs fetch");
+
+            sw.Reset();
+            sw.Start();
+            //for (int i = 0; i < 1000; i++)
+            //{
+            //    foreach (var item in list)
+            //    {
+            //        string a = d[item];
+            //    }
+            //}
+            sw.Stop();
+            Console.WriteLine(sw.Elapsed + " theirs fetch");
+
+            sw.Reset();
+            sw.Start();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                foreach (var item in list)
+                {
+                    string a = ht.Get(item);
+                }
+            }
+            sw.Stop();
+            Console.WriteLine(sw.Elapsed + " ours fetch");
+
+
+            
+            //   ht.Print();
+            Console.WriteLine("done");
+            Console.ReadLine();
+
+        }
+
+        static public List<string> LoadList(string file)
+        {
+            var list = new List<string>();
+            using (StreamReader sr = new StreamReader(file))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    list.Add(line);
+                }
+                return list;
+            }
+        }
+
+
+    }
+}
